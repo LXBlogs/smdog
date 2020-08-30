5e751b77893984814aa1657956545df9cd65c7f6
uakora <uakora96@gmail.com>
2020-08-30 22:58:44 +0800

    登录页布局调整

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,150 +1,155 @@
 <template>
-  <div id="page_login">
-    <header class="header">梦开始的地方</header>
-    <div class="login-wrap">
-      <div class="login-img"></div>
-      <div class="login-panel">
-        <h3 style="color: teal;">超级管理员登录页面</h3>
-        <div class="corner-ewm"></div>
-        <el-tabs
-          v-model="defaultLoginMethod"
-          @tab-click="handleTabClick"
-          type="card"
-        >
-          <el-tab-pane label="密码登录" name="passwordLogin">
-            <el-form
-              ref="passwordLoginForm"
-              :model="passwordLoginForm"
-              :rules="passwordLoginFormRules"
-              status-icon
-            >
-              <el-form-item prop="account">
-                <el-input
-                  size="medium"
-                  prefix-icon="el-icon-user"
-                  v-model="passwordLoginForm.account"
-                  placeholder="请输入账号"
-                />
-              </el-form-item>
-              <el-form-item prop="password">
-                <el-input
-                  size="medium"
-                  prefix-icon="el-icon-key"
-                  show-password
-                  v-model="passwordLoginForm.password"
-                  placeholder="请输入密码"
-                />
-              </el-form-item>
-              <el-form-item style="margin-bottom: 0;">
-                <el-col :span="13">
+  <el-container>
+    <el-header>{{ title }}</el-header>
+    <el-main>
+      <div class="login-wrap">
+        <div class="login-img"></div>
+        <div class="login-panel">
+          <h3 style="color: teal;">超级管理员登录页面</h3>
+          <div class="corner-ewm"></div>
+          <el-tabs
+            v-model="defaultLoginMethod"
+            @tab-click="handleTabClick"
+            type="card"
+          >
+            <el-tab-pane label="密码登录" name="passwordLogin">
+              <el-form
+                ref="passwordLoginForm"
+                :model="passwordLoginForm"
+                :rules="passwordLoginFormRules"
+                status-icon
+              >
+                <el-form-item prop="account">
                   <el-input
                     size="medium"
-                    prefix-icon="iconfont font-safe2"
-                    v-model="passwordLoginForm.verifyCode"
-                    placeholder="请输入验证码"
+                    prefix-icon="el-icon-user"
+                    v-model="passwordLoginForm.account"
+                    placeholder="请输入账号"
                   />
-                </el-col>
-                <el-col :span="10" :push="1">
+                </el-form-item>
+                <el-form-item prop="password">
                   <el-input
                     size="medium"
-                    v-model="passwordLoginForm.codeImage"
-                    placeholder="验证图片"
+                    prefix-icon="el-icon-key"
+                    show-password
+                    v-model="passwordLoginForm.password"
+                    placeholder="请输入密码"
                   />
-                </el-col>
-              </el-form-item>
-              <el-form-item>
-                <el-checkbox label="下次自动登录"></el-checkbox>
-              </el-form-item>
-              <el-form-item style="margin-bottom: 0;">
-                <el-button
-                  type="primary"
-                  size="medium"
-                  round
-                  @click="submitForm('passwordLoginForm')"
-                  style="width: 100%"
-                >
-                  登 录
-                </el-button>
-              </el-form-item>
-              <el-form-item>
-                <el-col class="text-right" :span="11">
+                </el-form-item>
+                <el-form-item style="margin-bottom: 0;">
+                  <el-col :span="13">
+                    <el-input
+                      size="medium"
+                      prefix-icon="iconfont font-safe2"
+                      v-model="passwordLoginForm.verifyCode"
+                      placeholder="请输入验证码"
+                    />
+                  </el-col>
+                  <el-col :span="10" :push="1">
+                    <el-input
+                      size="medium"
+                      v-model="passwordLoginForm.codeImage"
+                      placeholder="验证图片"
+                    />
+                  </el-col>
+                </el-form-item>
+                <el-form-item>
+                  <el-checkbox label="下次自动登录"></el-checkbox>
+                </el-form-item>
+                <el-form-item style="margin-bottom: 0;">
+                  <el-button
+                    type="primary"
+                    size="medium"
+                    round
+                    @click="submitForm('passwordLoginForm')"
+                    style="width: 100%"
+                  >
+                    登 录
+                  </el-button>
+                </el-form-item>
+                <el-form-item>
+                  <el-col class="text-right" :span="11">
+                    <el-link :underline="false" type="primary"
+                      >立即注册</el-link
+                    >
+                  </el-col>
+                  <el-col class="text-center" :span="2">
+                    <el-link :underline="false" type="primary">|</el-link>
+                  </el-col>
+                  <el-col :span="11">
+                    <el-link :underline="false" type="primary"
+                      >忘记密码？</el-link
+                    >
+                  </el-col>
+                </el-form-item>
+              </el-form>
+            </el-tab-pane>
+
+            <el-tab-pane label="短信登录" name="msgLogin">
+              <el-form>
+                <el-form-item>
+                  <el-input placeholder="手机号" size="medium">
+                    <el-select
+                      v-model="phonePrefix"
+                      slot="prepend"
+                      placeholder="请选择"
+                    >
+                      <el-option label="+86 中国大陆" value="1"></el-option>
+                      <el-option label="+852 中国香港" value="2"></el-option>
+                      <el-option label="+853 中国澳门" value="3"></el-option>
+                      <el-option label="+86" value="1"></el-option>
+                    </el-select>
+                  </el-input>
+                </el-form-item>
+                <el-form-item>
+                  <el-input placeholder="请输入验证码" size="medium">
+                    <el-button slot="append">获取验证码</el-button>
+                  </el-input>
+                </el-form-item>
+                <el-form-item>
+                  <el-button
+                    type="primary"
+                    size="medium"
+                    round
+                    @click="submitForm('loginForm')"
+                    style="width: 100%"
+                  >
+                    登 录
+                  </el-button>
+                </el-form-item>
+                <el-form-item class="text-right">
                   <el-link :underline="false" type="primary">立即注册</el-link>
-                </el-col>
-                <el-col class="text-center" :span="2">
-                  <el-link :underline="false" type="primary">|</el-link>
-                </el-col>
-                <el-col :span="11">
-                  <el-link :underline="false" type="primary"
-                    >忘记密码？</el-link
-                  >
-                </el-col>
-              </el-form-item>
-            </el-form>
-          </el-tab-pane>
+                </el-form-item>
+              </el-form>
+            </el-tab-pane>
 
-          <el-tab-pane label="短信登录" name="msgLogin">
-            <el-form>
-              <el-form-item>
-                <el-input placeholder="手机号" size="medium">
-                  <el-select
-                    v-model="phonePrefix"
-                    slot="prepend"
-                    placeholder="请选择"
+            <el-tab-pane label="第三方登录" name="thirdLogin">
+              <el-form>
+                <el-form-item class="text-center">第三方账号登录</el-form-item>
+                <el-form-item class="text-center">
+                  <el-avatar
+                    class="login-qq"
+                    icon="iconfont font-qq1"
+                    :size="40"
+                  ></el-avatar>
+                  <el-avatar class="login-wx" icon="iconfont font-wx" :size="40"
+                    >WX</el-avatar
                   >
-                    <el-option label="+86 中国大陆" value="1"></el-option>
-                    <el-option label="+852 中国香港" value="2"></el-option>
-                    <el-option label="+853 中国澳门" value="3"></el-option>
-                    <el-option label="+86" value="1"></el-option>
-                  </el-select>
-                </el-input>
-              </el-form-item>
-              <el-form-item>
-                <el-input placeholder="请输入验证码" size="medium">
-                  <el-button slot="append">获取验证码</el-button>
-                </el-input>
-              </el-form-item>
-              <el-form-item>
-                <el-button
-                  type="primary"
-                  size="medium"
-                  round
-                  @click="submitForm('loginForm')"
-                  style="width: 100%"
-                >
-                  登 录
-                </el-button>
-              </el-form-item>
-              <el-form-item class="text-right">
-                <el-link :underline="false" type="primary">立即注册</el-link>
-              </el-form-item>
-            </el-form>
-          </el-tab-pane>
-
-          <el-tab-pane label="第三方登录" name="thirdLogin">
-            <el-form>
-              <el-form-item class="text-center">第三方账号登录</el-form-item>
-              <el-form-item class="text-center">
-                <el-avatar
-                  class="login-qq"
-                  icon="iconfont font-qq1"
-                  :size="40"
-                ></el-avatar>
-                <el-avatar class="login-wx" icon="iconfont font-wx" :size="40"
-                  >WX</el-avatar
-                >
-                <el-avatar
-                  class="login-github"
-                  icon="iconfont font-github"
-                  :size="40"
-                  >Github</el-avatar
-                >
-              </el-form-item>
-            </el-form>
-          </el-tab-pane>
-        </el-tabs>
+                  <el-avatar
+                    class="login-github"
+                    icon="iconfont font-github"
+                    :size="40"
+                    >Github</el-avatar
+                  >
+                </el-form-item>
+              </el-form>
+            </el-tab-pane>
+          </el-tabs>
+        </div>
       </div>
-    </div>
-  </div>
+    </el-main>
+    <el-footer>{{ footer }}</el-footer>
+  </el-container>
 </template>
 
 <script>
@@ -173,6 +178,9 @@ export default {
       }
     };
     return {
+      title: '代码改变世界',
+      footer:
+        '有志者事竟成，破釜沉舟，百二秦关终属楚；苦心人天不负，卧薪尝胆，三千越甲可吞吴。',
       defaultLoginMethod: 'passwordLogin',
       passwordLoginForm: {
         account: '',
@@ -221,30 +229,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#page_login {
+.el-container {
   height: 100%;
   // background: radial-gradient(ellipse at center, #2d9b95 0%, #0e1329 100%);
   background-image: url('~@/assets/images/bg/wallhaven-2eqpzm.png');
   background-repeat: no-repeat;
   background-size: cover;
 
-  .header {
-    position: relative;
-    margin: 0;
-    padding: 32px 38px;
-    color: magenta;
+  .el-header {
+    line-height: 60px;
+    color: purple;
     font-family: cursive;
-    font-size: xxx-large;
+    font-size: x-large;
   }
   .login-wrap {
-    position: relative;
+    position: absolute;
     display: flex;
     width: 788px;
     height: 432px;
-    top: calc(50% - 120px);
-    left: 65%;
-    transform: translateX(-50%) translateY(-50%);
-    background: linear-gradient(45deg, #9a444e, #e06267);
+    top: 50%;
+    right: 5%;
+    transform: translateY(-50%);
+    // background: linear-gradient(45deg, #9a444e, #e06267);
     background: #fff;
     border-radius: 6px 0 6px 6px;
     .login-img {
@@ -300,6 +306,11 @@ export default {
         }
       }
     }
+  }
+  .el-footer {
+    line-height: 60px;
+    color: darkgray;
+    text-align: center;
   }
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-  // baseUrl: './dist', // 配置打包路径
   devServer: {
-    port: 3000, // 代理端口
+    port: 2234, // 端口
     open: true, // 项目启动时是否自动打开浏览器
     proxy: {
       '/api': {
         target: 'http://192.168.199.131:3000', // 目标地址--api路径
-        ws: true, //// 是否启用websockets
-        changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
-        // pathRewrite: { '^/api': 'http://192.168.199.131:3000/api' } //这里重写路径--vue端口
-        pathRewrite: { '^/api': '/api' } //这里重写路径--vue端口
+        ws: true, //// 启用 websockets
+        changeOrigin: true,
+        pathRewrite: { '^/api': '/api' }
       }
     }
   }
